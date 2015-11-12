a315f866bd9337e5166336abf1d946da5a17b68d
EnerccioCLI <enerccio@paldopice.com>
2015-11-12 11:55:34 +0100

    fixed test bug

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,20 +1,12 @@
-import cz.upol.vanusanik.disindent.compiler.CompilationResult;
-import cz.upol.vanusanik.disindent.compiler.DisindentCompiler;
-import cz.upol.vanusanik.disindent.parser.ParserBuilder;
 import cz.upol.vanusanik.disindent.runtime.DisindentClassLoader;
-
 
 public class Test {
 
 	public static void main(String[] args) throws Exception{
 		
-		ParserBuilder bd = new ParserBuilder();
-		bd.setFilename("test.din");
-		bd.setInput(Test.class.getResourceAsStream("test.din"));
-		DisindentCompiler c = new DisindentCompiler("test.din", bd.build(), new DisindentClassLoader(Test.class.getClassLoader()));
+		ClassLoader cl = new DisindentClassLoader(Test.class.getClassLoader());
 		
-		CompilationResult cr = c.compile();
-		Class<?> newClass = cr.getClass("test");
+		Class<?> newClass = cl.loadClass("test");
 		Object o = newClass.newInstance();
 		System.out.println(o);
 	}
