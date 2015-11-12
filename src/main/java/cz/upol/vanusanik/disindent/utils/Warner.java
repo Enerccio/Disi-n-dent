018b7261ae006497fddf3af75fc48a8ccd655e04
EnerccioCLI <enerccio@paldopice.com>
2015-11-12 12:23:45 +0100

    added warning system

---
+++ src/main/java/cz/upol/vanusanik/disindent/utils/Warner.java
@@ -0,0 +1,74 @@
+package cz.upol.vanusanik.disindent.utils;
+
+/**
+ * Helper utility that warns or throws exception if set to do so
+ * @author Peter Vanusanik
+ *
+ */
+public class Warner {
+	
+	/**
+	 * WarnLevel indicates what to do when warning happens
+	 * @author Peter Vanusanik
+	 *
+	 */
+	public enum WarnLevel {
+		/** Warning is ignored completely */
+		SILENT, 
+		/** Warning is logged to stderr */
+		WARN, 
+		/** Warning is thrown */
+		ERROR
+	}
+	
+	private static WarnLevel warnLevel = WarnLevel.WARN;
+	
+	/**
+	 * Sets the warn level to the desired level.
+	 * @param level
+	 */
+	public static void setWarnLevel(WarnLevel level){
+		warnLevel = level;
+	}
+	
+	/**
+	 * Creates a warning. The exception will be RuntimeException.
+	 * @param message warning message
+	 */
+	public static void warn(String message){
+		warn(new RuntimeException(message));
+	}
+	
+	/**
+	 * Creates a warning. This exception will be potentially thrown and message will be created from e.getMessage()
+	 * @param e exception to use as warning
+	 */
+	public static void warn(RuntimeException e){
+		try {
+			warn((Exception) e);
+		} catch (Exception e1) {
+			throw (RuntimeException)e1;
+		}
+	}
+
+	/**
+	 * Creates a warning. This exception will be potentially thrown and message will be created from e.getMessage()
+	 * @param e exception to use as warning
+	 */
+	public static void warn(Exception e) throws Exception {
+		if (warnLevel == WarnLevel.SILENT)
+			return;
+		
+		if (warnLevel == WarnLevel.WARN){
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			return;
+		}
+		
+		throw e;
+	}
+
+	private Warner(){
+		
+	}
+	
+}
